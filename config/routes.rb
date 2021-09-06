a9b908c939a60c21033c80b43c497a0a497aab14
Aki Wu <wuminzhe@gmail.com>
2021-09-06 12:29:09 +0800

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 end
