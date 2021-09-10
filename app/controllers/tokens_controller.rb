00c761fb04e3aea7a9749917ffbcd85695c15827
Aki Wu <wuminzhe@gmail.com>
2021-09-10 15:26:21 +0800

    token list sort by time

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,10 +1,12 @@
 class TokensController < ApplicationController
   def index
+    params[:q] = { "s" => "created_at desc" } if params[:q].nil? 
+    params[:q]["s"] = "created_at desc" if params[:q]["s"].blank?
+
     @q = Token.ransack(params[:q])
     query = @q.result
       .where(invalidated: false, token_uri_err: nil)
       .where.not(image: nil)
-      .order(created_at: :desc)
 
     @first_token_id = 
       if params[:first_token_id].blank?
@@ -14,12 +16,22 @@ class TokensController < ApplicationController
         params[:first_token_id]
       end
 
-    tokens = query
-      .where("tokens.id <= #{@first_token_id}")
+    if params[:q]["s"].include?(" desc")
+      tokens = query
+        .where("tokens.id <= #{@first_token_id}")
+    else
+      tokens = query
+        .where("tokens.id >= #{@first_token_id}")
+    end
 
     @pagy, @tokens = pagy(tokens, items: 120)
 
     @q_string = build_q_string(params[:q])
+    if @pagy.page == @pagy.last
+      @next_page_url = nil
+    else
+      @next_page_url = tokens_url(page: @pagy.next, first_token_id: @first_token_id) + "&" + @q_string
+    end
     @stream_name = build_stream_name(params[:q])
 
     respond_to do |format|
@@ -27,7 +39,7 @@ class TokensController < ApplicationController
       format.json {
         render json: { 
           entries: render_to_string(partial: "tokens", formats: [:html]),
-          next_page_url: @pagy.page == @pagy.last ? nil : tokens_url(page: @pagy.next, first_token_id: @first_token_id) + "&" + @q_string
+          next_page_url: @next_page_url
         }
       }
     end
@@ -40,16 +52,22 @@ class TokensController < ApplicationController
 
     result_arr = []
     if q["collection_blockchain_id_eq"].present?
-      result_arr << "q%5Bcollection_blockchain_id_eq%5D=#{q["collection_blockchain_id_eq"]}"
+      result_arr << "q[collection_blockchain_id_eq]=#{q["collection_blockchain_id_eq"]}"
     end
     if q["collection_nft_type_eq"].present?
-      result_arr << "q%5Bcollection_nft_type_eq%5D=#{q["collection_nft_type_eq"]}"
+      result_arr << "q[collection_nft_type_eq]=#{q["collection_nft_type_eq"]}"
+    end
+    if q["s"].present?
+      result_arr << "q[s]=#{q["s"]}"
     end
 
-    return result_arr.join("&") if result_arr.length > 1
     return "" if result_arr.length == 0
 
-    result_arr[0]
+    if result_arr.length > 1
+      result_arr.join("&") 
+    else
+      result_arr[0]
+    end
   end
 
   def build_stream_name(q)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  root 'tokens#index'
   resources :tokens
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
