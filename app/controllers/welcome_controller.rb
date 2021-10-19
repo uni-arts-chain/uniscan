299289fed5c730ebfede7dd0d3ea56fe7eb9a50b
Aki Wu <wuminzhe@gmail.com>
2021-10-19 10:58:15 +0800

    speedup index page by use joins and includes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,31 @@
 class WelcomeController < ApplicationController
   def index
     @tokens = Token.eligible
+      .includes(collection: [:blockchain])
+      .includes(:accounts)
+      .with_attached_image
       .order(created_at: :desc)
       .limit(50)
 
     # test
     @transfers = Transfer.joins(:token)
+      .includes(token: [:accounts, collection: [:blockchain], image_attachment: :blob])
+      .includes(:from, :to)
       .where("tokens.token_uri_processed=true and tokens.token_uri_err is null")
       .order("transfers.created_at desc")
       .limit(50)
 
     @highest_24h = Token.eligible
+      .includes(collection: [:blockchain])
+      .includes(:accounts)
+      .with_attached_image
       .order(transfers_count_24h: :desc)
       .limit(50)
 
     @highest_7d = Token.eligible
+      .includes(collection: [:blockchain])
+      .includes(:accounts)
+      .with_attached_image
       .order(transfers_count_7d: :desc)
       .limit(50)
 
