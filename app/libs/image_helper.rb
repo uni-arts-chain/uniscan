cc25527e87390d8e18fd3989c4c9a7045e091050
Aki Wu <wuminzhe@gmail.com>
2021-10-25 15:25:04 +0800

    update image helper

--- app/libs/image_helper.rb
+++ app/libs/image_helper.rb
@@ -8,24 +8,37 @@ class ImageHelper
   #
   # resize image size to 600x600.
   # keep the aspect ratio.
+  #
+  # It depends on the +imagemagick+ and +ffmpeg+.
+  # @param image_uri [String] the url of the image.
+  # @return [Tempfile, String, String] the file, the content type, and the original
+  # content type.
   def self.download_and_convert_image(image_uri)
-    tempfile = Down.download(image_uri, max_size: 50 * 1024 * 1024) # 50 MB
-    content_type = tempfile.content_type
-    ori_content_type = content_type
+    ori_tempfile = Down.download(image_uri, max_size: 50 * 1024 * 1024) # 50 MB
+    ori_content_type = tempfile.content_type
 
+    tempfile, content_type = convert(ori_tempfile, ori_content_type)
+    [ tempfile, content_type, ori_content_type ]
+  end
+
+  # Convert file
+  #
+  # 1. svg to png
+  # 2. video to gif
+  # 3. resize to 600x600 without changing the aspect ratio.
+  def self.convert(tempfile, content_type)
     # Convert svg to png
-    if ori_content_type.include?("svg")
-      tempfile = ImageProcessing::MiniMagick
-        .source(tempfile)
-        .convert("png")
-        .call
+    if content_type.include?("svg")
+
+      tempfile = svg_to_png(tempfile)
       content_type = "image/png"
-    end
 
     # Convert video to gif
-    if ori_content_type.start_with?("video")
+    elsif content_type.start_with?("video")
+
       tempfile = video_to_gif(tempfile)
       content_type = "image/gif"
+
     end
 
     # Shrink Larger Images
@@ -37,10 +50,24 @@ class ImageHelper
       .strip
       .call
 
-    [ tempfile, content_type, ori_content_type ]
+    [ tempfile, content_type ]
+  end
+
+  # svg to png converter
+  #
+  # @param svg_file [Tempfile] the svg file to convert
+  # @return [Tempfile]
+  def self.svg_to_png(svg_file)
+    ImageProcessing::MiniMagick
+      .source(svg_file)
+      .convert("png")
+      .call
   end
 
   # video to gif converter.
+  #
+  # @param video_file [Tempfile] the video file to convert
+  # @return [Tempfile]
   def self.video_to_gif(video_file)
     gif_file = Tempfile.new(["", ".gif"])
     `#{Rails.root.join("scripts", "vid-to-gif.sh").to_s} -f 10 -w 600 #{video_file.path} #{gif_file.path}`
